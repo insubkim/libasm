fd783a008015ca2c025f47a8386d02cbef3453a8
insub <j.j.kant1948@gmail.com>
2024-12-22 13:01:26 +0900

    Add ft_atoi_base

--- srcs/ft_atoi_base/ft_atoi_base.c
+++ srcs/ft_atoi_base/ft_atoi_base.c
@@ -1,20 +1,64 @@
+static int ft_strlen(const char *s)
+{
+    int len;
+
+    len = 0;
+    while (s && *s)
+    {
+        len++;
+        s++;
+    }
+    return len;
+}
+
+
+static int get_idx(const char* arr, char c)
+{
+    int idx;
+
+    idx = 0;
+    while (arr && *arr)
+    {
+        if (*arr == c)
+            return idx;
+        idx++;
+        arr++;
+    }
+    return -1;
+}
+
+static is_str_unique_chars(char *str)
+{
+    int chars[256] = {0, };
+
+    while (str && *str)
+    {
+        if (chars[*str] == 1)
+            return 0;
+        chars[*str] = 1;
+    }
+    return 1;
+}
+
 // 10 진수 까지만 가능
-int ft_atoi_base(const char* s, int base)
+int ft_atoi_base(const char* s, const char* base)
 {
     long long   integer;
     int         sign;
+    int         base_len;
 
     integer = 0;
     sign = 1;
-    if (s == 0)
-        return -1;
+    base_len = ft_strlen(base);
+    if (s == 0 || is_str_unique_chars(base))
+        return 0;
     while (*s && *s == ' ')
         s++;
     while (*s && (*s == '+' || *s =='-'))
         if (*s++ == '-')
             sign *= -1;
-    while (*s >= '0' && *s <= '0' + base - 1)
-        integer = integer * base + *s++ - '0';
+    while (contains(base, s))
+        integer = integer * base_len + get_idx(base, *s);
     return integer * sign;
 }
 
