616505c5f18bdd319f32e037e5d5a75cfda2000d
insub <j.j.kant1948@gmail.com>
2025-01-01 22:59:27 +0900

    study: get_errno_location

--- Study/Errno/main.c
+++ Study/Errno/main.c
@@ -8,11 +8,11 @@ int main()
     // 9 is for bad file descriptor
     printf ("Write Errno :%d\n", errno);
 
-    extern int _get_errorno(void);
+    extern int* _get_errorno(void);
     write(-1, "hello", 5);
-    int recent_errno = _get_errorno();
-    printf ("recent Errno :%d\n", recent_errno);
-    printf ("recent Errno :%d\n", __errno_location());
+    int *recent_errno = _get_errorno();
+    printf ("recent Errno :%d\n", *recent_errno);
+    printf ("recent Errno :%d\n", *__errno_location());
 
 
     return 0;
