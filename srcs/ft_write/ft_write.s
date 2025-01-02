0a2f057ce44582a46c201434b3e280ed6d6ecf71
insub <j.j.kant1948@gmail.com>
2025-01-02 23:31:43 +0900

    stoudy: syscal

--- srcs/ft_write/ft_write.s
+++ srcs/ft_write/ft_write.s
@@ -1,6 +1,7 @@
 section .text
 global _ft_write
 _ft_write:
-    mov rax, 0x2000004 ; use write() syscall 1 for linux, 0x2000004 for macos
+    ;mov rax, 0x2000004 ; use write() syscall 1 for linux, 0x2000004 for macos
+    mov rax, 1 ; use write() syscall 1 for linux, 0x2000004 for macos
     syscall
     ret
--- srcs/ft_write/main.c
+++ srcs/ft_write/main.c
@@ -2,19 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/syscall.h>
-
+#include <errno.h>
 
 int main()
 {
-    extern ssize_t ft_write(int fd, char *s, size_t count);
+    extern ssize_t _ft_write(int fd, char *s, size_t count);
     {
         char *s = "Hello Asm!\n";
 
-        ssize_t write_len1 = ft_write(1, s, strlen(s));
-        printf("ft_write_len :%zd\n", write_len1);
+        ssize_t write_len1 = _ft_write(1, s, strlen(s));
+        printf("_ft_write_len :%zd\nerrno :%d\n", write_len1, errno);
         
-        ssize_t write_len2 = write(1, s, strlen(s));
-        printf("write_len :%zd\n", write_len2);
+	ssize_t write_len2 = write(1, s, strlen(s));
+        printf("write_len :%zd\nerrno :%d\n", write_len2, errno);
+    }
+
+    {
+        char *s = "Hello Asm!\n";
+
+        ssize_t write_len1 = _ft_write(-1, s, strlen(s));
+        printf("_ft_write_len :%zd\nerrno :%d\n", write_len1, errno);
+        
+        ssize_t write_len2 = write(-1, s, strlen(s));
+        printf("write_len :%zd\nerrno :%d\n", write_len2, errno);
     }
     return 0;
 }
