5e942b2f66c36b50110f39fa37941dda8c972f7a
insub <j.j.kant1948@gmail.com>
2025-01-08 11:44:16 +0900

    ft_atoi_base finish

--- src_tests/ft_atoi_base/main.c
+++ src_tests/ft_atoi_base/main.c
@@ -5,6 +5,6 @@ int main()
 {
     extern int _ft_atoi_base(const char* str, const char* base);
 
-    printf("val : %d\n", _ft_atoi_base("FF", "0123456789ABCDEF"));
+    printf("val : %d\n", _ft_atoi_base("    +3611006", "01234567"));
     return 0;
 }
