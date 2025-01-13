3d8996c2052c332ca5de126c5813f907bfe5c3f0
insub <j.j.kant1948@gmail.com>
2025-01-13 23:57:02 +0900

    fixing: list sort

--- src_tests/ft_list/ft_list_sort.s
+++ src_tests/ft_list/ft_list_sort.s
@@ -3,32 +3,32 @@ global _ft_list_sort
 _ft_list_sort:
     ; check parameter
     cmp rdi, 0
-    jne bubble_sort
+    je return_error
     cmp rsi, 0
-    jne bubble_sort
+    je return_error
     mov r12, rsi
-    ret
 bubble_sort:
-    mov r8, 0 ; save outer loop prev node
-    mov r9, 0 ; save inner loop prev node
-    mov r10, [rdi] ; save head
-    mov rcx, r10 ; save outer loop start 
+    mov r8, 0		; save outer loop prev node
+    mov r9, 0     	; save inner loop prev node
+    mov r10, [rdi]	; save head
+    mov rcx, r10	; save outer loop start 
 outer_loop:
-    mov rax, [rcx + 8] 
-    cmp rax, 0 ; check if next is null
-    jne not_sorted ; if next is present it can be sorted
-    mov [rdi], r10 ; change head
+    mov rax, [rcx + 8]
+    mov r9, rcx		; set inner loop prev node 
+    cmp rax, 0		; check if next is null
+    jne not_sorted	; if next is present it can be sorted
+    mov [rdi], r10	; change head
     ret
 not_sorted:
-    mov rbx, [rcx + 8] ; save list next pointer
+    mov rbx, [rcx + 8]	; save list next pointer
 inner_loop:
-    cmp rbx, 0 ; check if list is null
-    jne compare ; if not null than compare 
-    mov r8, rcx ; save node
-    mov rcx, [rcx + 8] ; mov rcx to next node 
+    cmp rbx, 0 		; check if list is null
+    jne compare 	; if not null than compare 
+    mov r8, rcx 	; save node
+    mov rcx, [rcx + 8] 	; mov rcx to next node 
     jmp outer_loop
 compare:
-    push rbx ; save registers
+    push rbx 		; save registers
     push rcx
     push rdi
     push rsi
@@ -41,8 +41,7 @@ compare:
     sub rsp, 8
     mov rdi, rcx
     mov rsi, rbx
-    call r12 ; call cmp
-    pop rbp 
+    call r12 		; call cmp
     pop rsp
     pop r12
     pop r11
@@ -53,25 +52,38 @@ compare:
     pop rdi
     pop rcx
     pop rbx
-    pop rax
-    cmp rax, 0 ; check if first arg is bigger
+    cmp rax, 0 		; check if first arg is bigger
     jne dont_change_val
     ;change value
-    mov rax, [rcx + 8] ; save outer next  
-    mov r11, [rbx + 8] ; save inner next
-    mov [rbx + 8], rax ; 1,2 ; change inner next to outer next
-
-    mov [rcx + 8], r11 ; 3,4 ; change outer next to inner next
+    mov rax, [rcx + 8] 	; save outer next  
+    mov r11, [rbx + 8] 	; save inner next
     
-    cmp r9, 0
-    je l1
-    mov [r9 + 8], rcx ; 5, 6 ; change inner prev next to outer
-l1:
+    mov [rbx + 8], rax 	; change inner next to outer next
+    mov [rcx + 8], r11 	; change outer next to inner next
+  
     cmp r8, 0
-    je dont_change_val
-    mov [r8 + 8], rbx ; 7, 8 ; change puter prev next to inner
-
-dont_change_val:
-    mov r9, rbx ; save node
-    mov rbx, [rbx + 8] ; mov node to next
+    je change_root 
+    mov [r8 + 8], rbx
+    mov [r9 + 8], rcx
+    jmp exchange     
+change_root: 
+    mov r10, rbx
+    cmp r9, rcx
+    je c1
+    mov [r9 + 8], rcx
+c1:
+    mov [rbx + 8], rcx
+exchange:
+    mov rax, rbx
+    mov rbx, rcx
+    mov rcx, rax
+    mov r9, rbx
+    mov rbx, [rbx + 8]
     jmp inner_loop
+dont_change_val:
+    mov r9, rbx
+    mov rbx, [rbx + 8]
+    jmp inner_loop
+return_error:
+    mov rax, 0
+    ret
