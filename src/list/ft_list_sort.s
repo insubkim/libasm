69ebb94bb031d635f6edc2fc49cbe86da5625e9a
insub <j.j.kant1948@gmail.com>
2025-01-19 22:45:30 +0900

    add: ft_list_remove_if

---
+++ src/list/ft_list_sort.s
@@ -0,0 +1,47 @@
+section .text
+global _ft_list_sort
+_ft_list_sort:
+    ; check parameter
+    cmp rdi, 0
+    je return_error
+    cmp rsi, 0
+    je return_error
+    mov r12, rsi
+bubble_sort:
+    mov rcx, [rdi]	; save head
+outer_loop:
+    mov rax, [rcx + 8] ; get next node 
+    cmp rax, 0		; check if next is null
+    jne not_sorted	; if next is present it can be sorted
+    ret
+not_sorted:
+    mov rbx, [rcx + 8]	; save list next pointer
+inner_loop:
+    cmp rbx, 0 		; check if list is null
+    jne compare 	; if not null than compare 
+    mov rcx, [rcx + 8] 	; mov rcx to next node 
+    jmp outer_loop
+compare:
+    push rbx 		; save registers
+    push rcx
+    push rsp
+    and rsp, -16
+    mov rdi, [rcx]
+    mov rsi, [rbx]
+    call r12 		; call cmp
+    pop rsp
+    pop rcx
+    pop rbx
+    cmp eax, 0 		; check if first arg is bigger
+    jle dont_change_val
+    ;change value
+    mov r8, [rcx]  
+    mov r9, [rbx]
+    mov [rcx], r9
+    mov [rbx], r8
+dont_change_val:
+    mov rbx, [rbx + 8]
+    jmp inner_loop
+return_error:
+    mov rax, 0
+    ret
--- src_tests/ft_list/main.c
+++ src_tests/ft_list/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct s_list
 {
@@ -16,70 +17,70 @@ void    iter(t_list *head)
     }
 }
 
-void ft_list_sort(t_list** head, int (*cmp)())
-{
-	if (!head || !*head || !cmp)
-		return ;
-	t_list	*start = *head;
-	t_list	*n = *head;
-	t_list	*n_prev = 0;
-	while (n)
-	{
-		t_list	*p = n -> next;
-		t_list	*p_prev = n;
-		while (p)
-		{
-			if (cmp(n -> data, p -> data < 0) > 0)
-			{
-				// change n, p
-				// 1. a -> n -> b -> p 
-				if (n_prev != 0 && p_prev != n)
-				{
-					t_list *n_next = n -> next;
-					n -> next = p -> next;
-					p -> next = n_next;
-					n_prev -> next = p;
-					p_prev -> next = n;
-				}
-				// 2. a -> n -> p
-				if (n_prev != 0 && p_prev == n)
-				{
-					n -> next = p -> next;
-					p -> next = n;
-					n_prev -> next = p;
-				}
-				// 3. 0 -> n -> b -> p
-				if (n_prev == 0 && p_prev != n)
-				{
-					t_list *n_next = n -> next;
-					n -> next = p -> next;
-					p -> next = n_next;
-					p_prev -> next = n;
-					start = p;
-				}	
-				// 4. 0 -> n -> p
-				if (n_prev == 0 && p_prev == n)
-				{
-					n -> next = p -> next;
-					p -> next = n;
-					start = p;
-				}
-				t_list *tmp = n;
-				n = p;
-				p = tmp;
+// void ft_list_sort(t_list** head, int (*cmp)())
+// {
+// 	if (!head || !*head || !cmp)
+// 		return ;
+// 	t_list	*start = *head;
+// 	t_list	*n = *head;
+// 	t_list	*n_prev = 0;
+// 	while (n)
+// 	{
+// 		t_list	*p = n -> next;
+// 		t_list	*p_prev = n;
+// 		while (p)
+// 		{
+// 			if (cmp(n -> data, p -> data < 0) > 0)
+// 			{
+// 				// change n, p
+// 				// 1. a -> n -> b -> p 
+// 				if (n_prev != 0 && p_prev != n)
+// 				{
+// 					t_list *n_next = n -> next;
+// 					n -> next = p -> next;
+// 					p -> next = n_next;
+// 					n_prev -> next = p;
+// 					p_prev -> next = n;
+// 				}
+// 				// 2. a -> n -> p
+// 				if (n_prev != 0 && p_prev == n)
+// 				{
+// 					n -> next = p -> next;
+// 					p -> next = n;
+// 					n_prev -> next = p;
+// 				}
+// 				// 3. 0 -> n -> b -> p
+// 				if (n_prev == 0 && p_prev != n)
+// 				{
+// 					t_list *n_next = n -> next;
+// 					n -> next = p -> next;
+// 					p -> next = n_next;
+// 					p_prev -> next = n;
+// 					start = p;
+// 				}	
+// 				// 4. 0 -> n -> p
+// 				if (n_prev == 0 && p_prev == n)
+// 				{
+// 					n -> next = p -> next;
+// 					p -> next = n;
+// 					start = p;
+// 				}
+// 				t_list *tmp = n;
+// 				n = p;
+// 				p = tmp;
 
-				tmp = n_prev;
-				n_prev = p_prev;
-				p_prev = tmp;
-			}
-			p_prev = p;
-			p = p -> next;
-		}
-		n_prev = n;
-		n = n -> next;
-	}
-	*head = start;
-}
+// 				tmp = n_prev;
+// 				n_prev = p_prev;
+// 				p_prev = tmp;
+// 			}
+// 			p_prev = p;
+// 			p = p -> next;
+// 		}
+// 		n_prev = n;
+// 		n = n -> next;
+// 	}
+// 	*head = start;
+// }
 
 int main(void)
 {
@@ -108,43 +109,44 @@ int main(void)
     iter(head);
 
     
-    _ft_list_push_front(&head, "7");
-    _ft_list_push_front(&head, "6");
-    _ft_list_push_front(&head, "5");
-    _ft_list_push_front(&head, "4");
-    _ft_list_push_front(&head, "3");
-    _ft_list_push_front(&head, "8");
-    _ft_list_push_front(&head, "2");
-    _ft_list_push_front(&head, "a");
-    _ft_list_push_front(&head, "z");
-    _ft_list_push_front(&head, "d");
-    _ft_list_push_front(&head, "e");
-    _ft_list_push_front(&head, "r");
-    _ft_list_push_front(&head, "10");
+//     _ft_list_push_front(&head, "7");
+//     _ft_list_push_front(&head, "6");
+//     _ft_list_push_front(&head, "5");
+//     _ft_list_push_front(&head, "4");
+//     _ft_list_push_front(&head, "3");
+//     _ft_list_push_front(&head, "8");
+//     _ft_list_push_front(&head, "2");
+//     _ft_list_push_front(&head, "a");
+//     _ft_list_push_front(&head, "z");
+//     _ft_list_push_front(&head, "d");
+//     _ft_list_push_front(&head, "e");
+//     _ft_list_push_front(&head, "r");
+//     _ft_list_push_front(&head, "10");
 
-    printf("Before Sort\n");
-    iter(head);
+//     printf("Before Sort\n");
+//     iter(head);
 
-    _ft_list_sort(&head, strcmp);
-//     ft_list_sort(&head, strcmp);
+//     _ft_list_sort(&head, strcmp);
+// //     ft_list_sort(&head, strcmp);
     
-    printf("After Sort\n");
+//     printf("After Sort\n");
+//     iter(head);
+
+    extern void _ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+
+    char *data_ref = "freeable";
+    char *data = malloc(sizeof(char) * (strlen(data_ref) + 1));
+    memcpy(data, data_ref, strlen(data_ref));
+
+    _ft_list_push_front(&head, data);
+
+    printf("before remove\n");
     iter(head);
 
-    // extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+    _ft_list_remove_if(&head, data_ref, strcmp, free);
 
-    // char *data_ref = "freeable";
-    // char *data = malloc(sizeof(char) * (strlen(data_ref) + 1));
-
-    // _ft_list_push_front(&head, data);
-
-    // printf("before remove\n");
-    // iter(head);
-
-    // _ft_list_remove_if(&head, data_ref, strcmp, free);
-
-    // printf("after remove\n");
-    // iter(head);
+    printf("after remove\n");
+    iter(head);
 
     return 0;
 }
